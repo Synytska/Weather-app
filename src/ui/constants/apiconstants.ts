b91cb2d7486c54f8ccf5b6371aec051b1a0cd570
khrystyna <pankiv38@gmail.com>
2024-03-01 16:35:28 +0000

    fix env

--- src/ui/constants/apiconstants.ts
+++ src/ui/constants/apiconstants.ts
@@ -1,11 +1,11 @@
 export const geoApiUrl = 'https://wft-geo-db.p.rapidapi.com/v1/geo/cities?minPopulation=10000&namePrefix=';
 
 export const headers = {
-    'X-RapidAPI-Key': '943550a715msh1c0c4f36803caf5p1bc896jsn096b2e012a0b',
+    'X-RapidAPI-Key': process.env.NEXT_PUBLIC_GEO_KEY,
     'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
 };
 
-export const WEATHER_API_URL = 'https://api.openweathermap.org/data/3.0';
+export const WEATHER_API_URL = process.env.NEXT_PUBLIC_API_URL;
 export const DEFAULT_WEATHER_API_URL = 'https://api.openweathermap.org/data/3.0/onecall?lat=50.4333&lon=30.5167';
 export const DEFAULT_FORECAST_API_URL = 'https://api.openweathermap.org/data/2.5/forecast?q=Kyiv';
-export const WEATHER_API_KEY = '5e1dbfef74af01c8468028e63b0db4cb';
+export const WEATHER_API_KEY = process.env.NEXT_PUBLIC_API_KEY;
