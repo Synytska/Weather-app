ff0fc4cbbe2d11d4771d38a97c4268a170098274
khrystyna <pankiv38@gmail.com>
2024-03-15 12:40:20 +0000

    fixed secwrap

--- src/ui/modules/weather/weatherBlocks/ForecastWeather/components/ForecastSecondWrap.tsx
+++ src/ui/modules/weather/weatherBlocks/ForecastWeather/components/ForecastSecondWrap.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react';
 
 import { Icons } from '@/src/ui/components/Icons';
 
-import { WEATHER_TYPE } from '../constants/forecastconst';
+import { WEATHER_TYPE } from '@/src/ui/constants/mainconstants';
 
 interface ISecWrap {
     main: WEATHER_TYPE;
